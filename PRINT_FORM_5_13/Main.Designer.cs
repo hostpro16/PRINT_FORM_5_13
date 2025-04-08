858d8dbbd25c44e8b629f00fca69b3b2e8a8ec2e
hostpro16 <hostpro16@gmail.com>
2025-04-08 17:56:07 +0200

    Add project files.

---
+++ PRINT_FORM_5_13/Main.Designer.cs
@@ -0,0 +1,70 @@
+﻿
+namespace PRINT_FORM_5_13
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrint.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.25F);
+            this.btnPrint.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.btnPrint.Location = new System.Drawing.Point(36, 34);
+            this.btnPrint.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(358, 148);
+            this.btnPrint.TabIndex = 0;
+            this.btnPrint.Text = "IN";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 225);
+            this.Controls.Add(this.btnPrint);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Main";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrint;
+    }
+}
+
---
+++ PRINT_FORM_5_13/Main.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRINT_FORM_5_13
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            // Tạo DataTable
+            DataTable dt = new DataTable();
+            dt.Columns.Add("ean", typeof(string));
+            dt.Columns.Add("mnozstvi", typeof(string));
+            dt.Columns.Add("hsd", typeof(string));
+
+            // Thêm thủ công từng dòng dữ liệu
+            dt.Rows.Add("02114111", "9", "12.05.2025");
+            dt.Rows.Add("05064900", "2", "09.04.2025");
+            dt.Rows.Add("05065450", "18", "09.04.2025");
+            dt.Rows.Add("05062690", "23", "10.04.2025");
+            dt.Rows.Add("05062691", "103", "10.04.2025");
+            dt.Rows.Add("05063697", "1", "10.04.2025");
+            dt.Rows.Add("05063698", "1", "10.04.2025");
+            dt.Rows.Add("05065071", "1", "10.04.2025");
+            dt.Rows.Add("05060065", "1", "11.04.2025");
+            dt.Rows.Add("05060072", "1", "11.04.2025");
+            dt.Rows.Add("05063578", "3", "11.04.2025");
+            dt.Rows.Add("05064906", "2", "11.04.2025");
+            dt.Rows.Add("05061883", "1", "21.04.2025");
+            dt.Rows.Add("05061898", "1", "21.04.2025");
+            dt.Rows.Add("05060383", "4", "22.04.2025");
+            dt.Rows.Add("01020563", "48", "14.04.2025");
+            dt.Rows.Add("01014222", "48", "16.04.2025");
+            dt.Rows.Add("01014223", "240", "17.04.2025");
+            dt.Rows.Add("01014387", "120", "22.04.2025");
+            dt.Rows.Add("01014385", "320", "22.04.2025");
+            dt.Rows.Add("01080014", "24", "23.04.2025");
+            dt.Rows.Add("01010884", "102", "30.04.2025");
+            dt.Rows.Add("01012545", "120", "30.04.2025");
+            dt.Rows.Add("01012101", "48", "30.04.2025");
+            dt.Rows.Add("01014199", "310", "30.04.2025");
+            dt.Rows.Add("01014200", "24", "30.04.2025");
+            dt.Rows.Add("01014366", "1500", "31.05.2025");
+
+
+            //XỬ LÝ TIẾP VỚI DATATABLE
+            MessageBox.Show(dt.Rows.Count.ToString());
+
+        }
+    }
+}
